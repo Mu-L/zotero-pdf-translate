a0dea7d4a144abfe99b891474a002aff478e9496
Junming Chen <me@chapoly1305.com>
2026-04-21 06:33:44 -0400

    fix: bind abstract menu visibility to correct pref (#1413)
    * fix: bind abstract menu visibility to correct pref
    
    * fix: correct title/abstract item menu pref bindings

--- src/modules/menu.ts
+++ src/modules/menu.ts
@@ -56,7 +56,7 @@ export function registerMenu() {
         onShowing: (event, context) => {
           context.setVisible(
             !!(
-              getPref("showItemMenuTitleTranslation") &&
+              getPref("showItemMenuAbstractTranslation") &&
               context.items?.every((item) => item.isRegularItem())
             ),
           );
